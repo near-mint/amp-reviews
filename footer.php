d71aab48974de85e4c37c9ebb5be5897d4aa9d70
Matthew Clark <mclark@indeed.com>
2018-07-26 23:21:46 -0500

    various improvements to the header and the footer

--- footer.php
+++ footer.php
@@ -24,7 +24,7 @@
 			<span class="sep"> | </span>
 				<?php
 				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'amp_reviews' ), 'amp_reviews', '<a href="https://automattic.com/">Automattic</a>' );
+				printf( esc_html__( 'Theme: %1$s by %2$s.', 'amp_reviews' ), 'amp_reviews', '<a href="http://whoismattclark.com/">Matthew Clark</a>' );
 				?>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
